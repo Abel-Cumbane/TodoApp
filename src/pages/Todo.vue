fc13aef083663159eaac1c1814475d1d80a4c9d7
Abel <abelrosac@gmail.com>
2022-06-25 20:33:17 +0100

    chore

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,42 +1,70 @@
 <template>
   <q-page class="bg-grey-3 column">
-    <q-list
-      class="bg-white"
-      separator
-      bordered
+    <div class="row q-pa-sm bg-primary">
+      <q-input
+        v-model="newTask"
+        @keypress.enter="addTask"
+        class="col"
+        square
+        filled
+        bg-color="white "
+        placeholder="Add task"
+        dense
       >
+        <template v-slot:append>
+          <q-btn
+          @click="addTask"
+           round
+           dense
+           flat
+           icon="add"
+        />
+        </template>
+      </q-input>
+    </div>
+    <q-list class="bg-white" separator bordered>
       <q-item
-        v-for="(task, index)  in tasks"
+        v-for="(task, index) in tasks"
         :key="task.title"
         @click="task.done = !task.done"
-        :class="{'done bg-blue-1':task.done}"
+        :class="{ 'done bg-blue-1': task.done }"
         clickable
-         v-ripple
+        v-ripple
       >
         <q-item-section avatar>
           <q-checkbox
             v-model="task.done"
             class="no-pointer-events"
-            color="primary" />
+            color="primary"
+          />
         </q-item-section>
         <q-item-section>
-          <q-item-label>{{task.title}}</q-item-label>
+          <q-item-label>{{ task.title }}</q-item-label>
         </q-item-section>
-        <q-item-section
-          v-if="task.done"
-          side
-        >
+        <q-item-section v-if="task.done" side>
           <q-btn
-            @click.stop = "deleteTask(index)"
+            @click.stop="deleteTask(index)"
             flat
             round
             dense
             color="primary"
             icon="delete"
-            />
+          />
         </q-item-section>
       </q-item>
     </q-list>
+    <div
+        v-if="!tasks.length"
+        class="no-tasks absolute-center">
+      <q-icon
+        name="check"
+        size="100px"
+        color="primary"
+      />
+      <div class="text-h5 text-primary text-center">
+        No tasks
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -44,34 +72,47 @@
 export default {
   data() {
     return {
-      tasks:[
-        {title:'Get bananas',
+      newTask: '',
+      tasks: [
+        // { title: "Get bananas", done: false },
+        // { title: "Eat bananas", done: false },
+        // { title: "Poo bannas", done: false },
+      ],
+    };
+  },
+  methods: {
+    deleteTask(index) {
+      this.$q
+        .dialog({
+          title: "Confirm",
+          message: "Desejas apagar?",
+          cancel: true,
+          persistent: true,
+        })
+        .onOk(() => {
+          this.tasks.splice(index, 1);
+          this.$q.notify("Task deleted");
+        });
+    },
+    addTask(){
+      this.tasks.push({
+        title: this.newTask,
         done: false
-        },
-        {title:'Eat bananas',
-        done: false
-        },
-        {title:'Poo bannas',
-        done: false
-        }
-      ]
+      })
+      this.newTask = ''
     }
   },
-  methods:{
-    deleteTask (index){
-      this.tasks.splice(index, 1)
-    }
-  }
-
-}
+};
 </script>
 
 <style lang="scss">
-  .done{
-    .q-item__label{
-      text-decoration: line-through;
-      color: #bbb;
-    }
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
   }
-
+.no-tasks {
+  opacity: 0.5;
+}
+}
 </style>
